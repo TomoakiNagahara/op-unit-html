5ba3375aa91d49292708225a5f5936210e936a11
Tomoaki iMac5K-1 <tomoaki.nagahara@gmail.com>
2019-02-07 18:32:28 +0900

    New: index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * unit-html:/index.php
+ *
+ * @creation  2018-01-24
+ * @version   1.0
+ * @package   unit-i18n
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+//	...
+include('Html.class.php');
+
+//	...
+return true;
