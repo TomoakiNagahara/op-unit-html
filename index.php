c5e9f7ae269b78671c6f9478132569b22ad062fe
Tomoaki mbp13r-1 <tomoaki.nagahara@gmail.com>
2019-03-24 17:25:07 +0900

    FIX: ROMANCE DAWN 2019

--- index.php
+++ index.php
@@ -4,12 +4,9 @@
  *
  * @creation  2018-01-24
  * @version   1.0
- * @package   unit-i18n
+ * @package   unit-html
  * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  * @copyright Tomoaki Nagahara All right reserved.
  */
 //	...
-include('Html.class.php');
-
-//	...
-return true;
+include(__DIR__.'/Html.class.php');
